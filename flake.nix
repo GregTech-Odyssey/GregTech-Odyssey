80bb4f900b22a7d9f5fe6dc6215a3c267976bd1e
alcox233 <113848416+alcox233@users.noreply.github.com>
2026-07-17 02:14:55 +0800

    fix: include README in server pack

--- flake.nix
+++ flake.nix
@@ -138,6 +138,7 @@
               cp ${./start-server.sh} $out/start-server.sh
               cp ${./start-server.bat} $out/start-server.bat
               cp ${./start-server.ps1} $out/start-server.ps1
+              cp ${./README-server.md} $out/README.md
               chmod +x $out/start-server.sh
 
               sed -i 's/\(unix_args.txt\) \("\$@"\)/\1 nogui \2/' $out/run.sh
